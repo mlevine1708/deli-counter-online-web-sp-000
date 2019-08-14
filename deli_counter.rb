4483402d374bca4de5ada5331ff6fc786d124ce4
Micah Levine <micah.levine@outlook.com>
2019-08-14 21:30:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ end
 def now_serving(line)
   if line.length == 0
     puts "There is nobody waiting to be served!"
-  else 
+  else
     puts "Currently serving #{line[0]}."
     line.shift
   end
